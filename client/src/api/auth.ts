fb7debb112ac397e3f84afef2900b898cf0d7ccb
wlewis0991 <wlewis1990@gmail.com>
2026-04-23 09:00:43 -0700

    Render backend URL updated to API location in frontend

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://blog-1p3r.onrender.com",
 });
 
 export async function login(username: string, password: string) {
